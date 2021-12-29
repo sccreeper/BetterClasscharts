b406d4d9115c41d9f23fe2e392941fe48e158fc0
Oscar Peace <oscar@oscarcp.net>
2021-12-29 17:29:28 +0000

    Sections won't show when there is no hw

--- src/handlers/display_homework_tiles.py
+++ src/handlers/display_homework_tiles.py
@@ -7,10 +7,18 @@ import globals
 
 
 def display_hw(*args):
+    
+    #Semi-globals
     time = datetime.now()
+
+    SHOW_TURNED_IN = True
+    SHOW_DUE = True
+    NO_HW = False
 
     data = globals.API_CLIENT.get_homework()
 
+    #Start of proc
+    
     globals.screen.ids.toolbar.title = "Homework" #Switch title
 
     if len(data) == globals.HOMEWORK_LENGTH:
@@ -35,46 +43,72 @@ def display_hw(*args):
         elif hw["status"]["ticked"] == "no" and t.time() < t.mktime(due_date.timetuple()): homework_lists["due"].append(hw)
         else: homework_lists["late"].append(hw)
     
-    globals.screen.ids.homework_screen_manager.get_screen("HomeworkScreen").ids.homework_scroll.data.append(
-            {
-                "viewclass" : "MDLabel",
-                "font_style" : "Button",
-                "text" : "Turned in",
-                "id" : "label_turned_in"
-            }
-    )
+    #See wether to show turned in or due.
+    if len(homework_lists["turned_in"]) == 0:
+        SHOW_TURNED_IN = False
+    
+    if len(homework_lists["due"] + homework_lists["late"]) == 0:
+        SHOW_DUE = False
 
-    for L in homework_lists:
-        
-        _hw_list = homework_lists[L]
+    if not SHOW_DUE and not SHOW_TURNED_IN:
+        NO_HW = True
 
-        if L == "due":
-                globals.screen.ids.homework_screen_manager.get_screen("HomeworkScreen").ids.homework_scroll.data.append(
-            {
-                "viewclass" : "MDLabel",
-                "id" : "label_due",
-                "font_style" : "Button",
-                "text" : "Due"
-            }
-    )
-
-        for hw in _hw_list:
-
-            due_date = hw["due_date"].split("-")
-            due_date = date(int(due_date[0]), int(due_date[1]), int(due_date[2]))
-
-            if due_date.day == time.day + 1:
-                due_date_string = "Tommorow"
-            else:
-                due_date_string = f"{DAY[due_date.weekday()]} {due_date.day}{friendly_date(due_date.day)} {MONTH[due_date.month - 1]} {'' if due_date.year == time.year else due_date.year}"
-                
+    NO_HW = True
+    
+    if not NO_HW:
+    
+        if SHOW_TURNED_IN: 
         
             globals.screen.ids.homework_screen_manager.get_screen("HomeworkScreen").ids.homework_scroll.data.append(
-                {
-                    "viewclass" : "HomeworkView",
-                    "hw_title" : hw["title"],
-                    "hw_subtitle" : hw["subject"] + " - Due: " + due_date_string,
-                    "hw_id" : hw["id"]
-
-                }
+                    {
+                        "viewclass" : "MDLabel",
+                        "font_style" : "Button",
+                        "text" : "Turned in",
+                        "id" : "label_turned_in"
+                    }
             )
+
+        for L in homework_lists:
+            
+            _hw_list = homework_lists[L]
+
+            if L == "due":
+                    globals.screen.ids.homework_screen_manager.get_screen("HomeworkScreen").ids.homework_scroll.data.append(
+                {
+                    "viewclass" : "MDLabel",
+                    "id" : "label_due",
+                    "font_style" : "Button",
+                    "text" : "Due"
+                }
+        )
+
+            for hw in _hw_list:
+
+                due_date = hw["due_date"].split("-")
+                due_date = date(int(due_date[0]), int(due_date[1]), int(due_date[2]))
+
+                if due_date.day == time.day + 1:
+                    due_date_string = "Tommorow"
+                else:
+                    due_date_string = f"{DAY[due_date.weekday()]} {due_date.day}{friendly_date(due_date.day)} {MONTH[due_date.month - 1]} {'' if due_date.year == time.year else due_date.year}"
+                    
+            
+                globals.screen.ids.homework_screen_manager.get_screen("HomeworkScreen").ids.homework_scroll.data.append(
+                    {
+                        "viewclass" : "HomeworkView",
+                        "hw_title" : hw["title"],
+                        "hw_subtitle" : hw["subject"] + " - Due: " + due_date_string,
+                        "hw_id" : hw["id"]
+
+                    }
+                )
+    #If there is no homework, show alternative message
+    else:
+        globals.screen.ids.homework_screen_manager.get_screen("HomeworkScreen").ids.homework_scroll.data.append(       
+            {
+                "viewclass" : "MDLabel",
+                "font_style" : "H5",
+                "halign" : "center",
+                "text" : "No homework!"
+            }
+        )

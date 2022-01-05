19e98948972fdb827f45acd9bdbd2c7a0ff8aee1
Oscar Peace <oscar@oscarcp.net>
2022-01-05 19:13:19 +0000

    Fix bug: Show today if due in today

--- src/handlers/display_homework_tiles.py
+++ src/handlers/display_homework_tiles.py
@@ -91,6 +91,8 @@ def display_hw(*args):
 
                 if due_date.day == time.day + 1:
                     due_date_string = "Tommorow"
+                if due_date.day == time.day:
+                    due_date_string = "Today"
                 else:
                     due_date_string = f"{DAY[due_date.weekday()]} {due_date.day}{friendly_date(due_date.day)} {MONTH[due_date.month - 1]} {'' if due_date.year == time.year else due_date.year}"
                     

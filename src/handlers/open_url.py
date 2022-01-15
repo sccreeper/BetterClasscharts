d94d5681333d847790edf3c58af2b90e924bb16d
Oscar Peace <oscar@oscarcp.net>
2022-01-15 12:36:23 +0000

    URL open on android

--- src/handlers/open_url.py
+++ src/handlers/open_url.py
@@ -6,32 +6,28 @@ import webbrowser
 if platform == 'android':
     from jnius import autoclass, cast
 
+    def open_url(url):
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        activity = PythonActivity.mActivity
+        Intent = autoclass('android.content.Intent')
+        Uri = autoclass('android.net.Uri')
+        browserIntent = Intent()
+        browserIntent.setAction(Intent.ACTION_VIEW)
+        browserIntent.setData(Uri.parse(url))
+        currentActivity = cast('android.app.Activity', activity)
+        currentActivity.startActivity(browserIntent)
+
+    # Web browser support.
+    class AndroidBrowser(object):
+        def open(self, url, new=0, autoraise=True):
+            open_url(url)
+        def open_new(self, url):
+            open_url(url)
+        def open_new_tab(self, url):
+            open_url(url)
+
+    webbrowser.register('android', AndroidBrowser, None)
+
 def launch_webbrowser(instance, url):
     
-    if platform == 'android':
-        
-        def open_url(url):
-            PythonActivity = autoclass('org.kivy.android.PythonActivity')
-            activity = PythonActivity.mActivity
-            Intent = autoclass('android.content.Intent')
-            Uri = autoclass('android.net.Uri')
-            browserIntent = Intent()
-            browserIntent.setAction(Intent.ACTION_VIEW)
-            browserIntent.setData(Uri.parse(url))
-            currentActivity = cast('android.app.Activity', activity)
-            currentActivity.startActivity(browserIntent)
-
-        # Web browser support.
-        class AndroidBrowser(object):
-            def open(self, url, new=0, autoraise=True):
-                open_url(url)
-            def open_new(self, url):
-                open_url(url)
-            def open_new_tab(self, url):
-                open_url(url)
-
-        webbrowser.register('android', AndroidBrowser, None, -1)
-    
-
     webbrowser.open(url)
-

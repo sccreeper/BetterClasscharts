5bc3234ffb05b844fa93b06478ddd8fd30d05d44
Oscar Peace <oscar@oscarcp.net>
2022-03-25 16:47:04 +0000

    Change all build scripts to bash and add release option

--- build.sh
+++ build.sh
@@ -1,16 +1,71 @@
 #!/bin/bash
 
+LICENSE_FILE="src/res/licenses.txt"
+
+declare -a LICENSE_URLS=("https://raw.githubusercontent.com/kivy/python-for-android/develop/LICENSE" "https://raw.githubusercontent.com/kivy/pyjnius/master/LICENSE" "https://raw.githubusercontent.com/kivy/kivy/master/LICENSE" "https://raw.githubusercontent.com/kivymd/KivyMD/master/LICENSE" "https://raw.githubusercontent.com/psf/requests/main/LICENSE" "https://raw.githubusercontent.com/python/cpython/main/LICENSE")
+declare -a LICENSE_NAMES=("Python For Android" "pyjnius" "Kivy" "KivyMD" "requests" "Python")
+
 echo -------------------
 echo Pulling licenses...
 echo -------------------
 
-python3 pull_licenses.py
+COUNT=0
+
+#Clear license file
+echo "" > ${LICENSE_FILE}
+
+for I in "${LICENSE_URLS[@]}" 
+do
+
+    echo -en "\r\033[KPulling license $((COUNT+1))/${#LICENSE_URLS[@]}"
+
+    echo -e "\n" >> ${LICENSE_FILE}
+    echo -e "-------------\n" >> "${LICENSE_FILE}"
+    echo -e "\n" >> ${LICENSE_FILE}
+    
+    echo -e "${LICENSE_NAMES[$COUNT]}\n" >> "${LICENSE_FILE}"
+
+    echo -e "\n" >> ${LICENSE_FILE}
+    echo -e "-------------\n" >> "${LICENSE_FILE}"
+    echo -e "\n" >> ${LICENSE_FILE}
+
+    echo "$(curl -s $I)\n" >> "${LICENSE_FILE}"
+
+    ((COUNT++))
+
+done
+
+echo -e "\nDone!"
 
 echo -------------------
 echo Building APK and deploying...
 echo -------------------
 
-buildozer android debug deploy run
+if [ "$1" == "debug" ]
+then
+
+    buildozer android debug deploy run
+
+else
+
+    if [ ! -e "uber-apk-signer*.apk" ] 
+    then
+
+        echo "Building release APK..."
+
+        buildozer android release
+
+        echo "Signing the APK(s)..."
+
+        java -jar $(find ./ -name "uber-apk-signer*.jar" -print -quit) --apks ./bin/
+    
+    else
+
+        echo "Unable to find uber-apk-signer!"
+        exit
+    fi
+
+fi
 
 echo -------------------
 echo Finished
